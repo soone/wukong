299bbdb574602eda64873695bf23bf829a06db5d
soone <fengyue15@163.com>
2014-12-18 19:34:53 +0800

    data home

--- xiaowu/myeye.py
+++ xiaowu/myeye.py
@@ -3,8 +3,8 @@
 import cv, Image
 
 capture = cv.CaptureFromCAM(0)
-cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 1024)
-cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 768)
+cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 600)
+cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 400)
 img = cv.QueryFrame(capture)
 pi = Image.fromstring("RGB", cv.GetSize(img), img.tostring())
 pi.save("./a.jpg", format = "JPEG")
