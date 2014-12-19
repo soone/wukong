93f5d3dde6772ce152676b2c81608143dad8594f
soone <fengyue15@163.com>
2014-12-19 13:31:52 +0800

    home data

--- xiaowu/myeye.py
+++ xiaowu/myeye.py
@@ -1,10 +1,12 @@
 #!/bin/env python
 
-import cv, Image
+import cv, Image, time
 
 capture = cv.CaptureFromCAM(0)
 cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 600)
 cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 400)
-img = cv.QueryFrame(capture)
-pi = Image.fromstring("RGB", cv.GetSize(img), img.tostring())
-pi.save("./a.jpg", format = "JPEG")
+for i in range(10):
+    img = cv.QueryFrame(capture)
+    pi = Image.fromstring("RGB", cv.GetSize(img), img.tostring())
+    pi.save("./pic/" + str(i) + ".jpg", format = "JPEG")
+    time.sleep(3)
